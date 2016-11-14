86e0d71dc40acbe1038077dbf38489c3017ae2e6
Theo Laurent <theo.laurent@ens.fr>
2016-11-14 14:14:55 +0100

    Switch to a Monad Transformer.

--- Cont.hs
+++ Cont.hs
@@ -2,38 +2,59 @@
 
 {-# LANGUAGE RankNTypes #-}
 
-module GenCont where
+module Cont where
 
+import Data.Functor.Identity
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
 import Control.Monad (ap, liftM)
 
+type ExtContT m a = forall w . ContT w m a
 
-newtype Cont w a = Cont { unCont :: (a -> w) -> w }
+newtype ContT w m a = ContT { unContT :: (a -> m w) -> m w }
 
-instance Monad (Cont w) where
-    return x = Cont $ \k -> k x
-    m >>= f  = Cont $ \k -> unCont m (\mv -> unCont (f mv) k)
+instance Monad (ContT w m) where
+    return x = ContT $ \k -> k x
+    m >>= f  = ContT $ \k -> unContT m (\mv -> unContT (f mv) k)
 
-instance Applicative (Cont w) where
+instance Applicative (ContT w m) where
   pure = return
   (<*>) = ap
 
-instance Functor (Cont w) where
+instance Functor (ContT w m) where
   fmap = liftM
 
 
 -- The type of the captured continuation
 -- Once again, it is not a function in the object language;
 -- it's result is NOT of the type Cont w x
-type K w a = a -> w
+type KT w m a = a -> m w
 
-callCC :: (K w a -> Cont w a) -> Cont w a
-callCC f = Cont $ \k -> unCont (f k) k
+callCCT :: (KT w m a -> ContT w m a) -> ContT w m a
+callCCT f = ContT $ \k -> unContT (f k) k
 
-throw :: K w a -> a -> Cont w b
-throw k x = Cont $ \ _ -> k x
+throwT :: KT w m a -> a -> ContT w m b
+throwT k x = ContT $ \ _ -> k x
 
 -- And here is the main difference from Cont: the higher-rank type of
 -- runCont, ensuring that the computation should really not have looked
 -- at the answer-type
-runCont :: (forall w. Cont w a) -> a
-runCont m = unCont m id
+runContT :: Monad m => (forall w. ContT w m a) -> m a
+runContT m = unContT m return
+
+instance MonadTrans (ContT r) where
+    lift m = ContT (m >>=)
+
+instance (MonadIO m) => MonadIO (ContT r m) where
+    liftIO = lift . liftIO
+
+
+-- type Cont w = ContT w Identity
+
+-- type K w a = a -> w
+
+-- callCC :: (K w a -> Cont w a) -> Cont w a
+-- callCC f = Cont $ \k -> unCont (f k) k
+
+-- throw :: K w a -> a -> Cont w b
+-- throw k x = Cont $ \ _ -> k x
