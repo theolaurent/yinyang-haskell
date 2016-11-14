d0f37d47298a682900528e2007e429382bd8a411
Theo Laurent <theo.laurent@ens.fr>
2016-11-14 14:32:52 +0100

    WIP Draft save

--- Cont.hs
+++ Cont.hs
@@ -4,57 +4,64 @@
 
 module Cont where
 
-import Data.Functor.Identity
-import Control.Monad.IO.Class
-import Control.Monad.Trans.Class
-import Control.Monad (ap, liftM)
+import           Control.Monad             (ap, liftM)
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Class
+import           Data.Functor.Identity
 
-type ExtContT m a = forall w . ContT w m a
 
-newtype ContT w m a = ContT { unContT :: (a -> m w) -> m w }
+-- newtype ContT w m a = ContT { unContT :: (a -> m w) -> m w }
+newtype Cont r a = Cont { unCont :: (a -> r) -> r }
 
-instance Monad (ContT w m) where
-    return x = ContT $ \k -> k x
-    m >>= f  = ContT $ \k -> unContT m (\mv -> unContT (f mv) k)
+instance Monad (Cont r) where
+    return x = Cont $ \k -> k x
+    m >>= f  = Cont $ \k -> unCont m (\mv -> unCont (f mv) k)
 
-instance Applicative (ContT w m) where
+instance Applicative (Cont r) where
   pure = return
   (<*>) = ap
 
-instance Functor (ContT w m) where
+instance Functor (Cont r) where
   fmap = liftM
 
 
 -- The type of the captured continuation
 -- Once again, it is not a function in the object language;
 -- it's result is NOT of the type Cont w x
-type KT w m a = a -> m w
+-- class K k where
+--   throw :: k w a -> a ->
+type K r a = a -> r
 
-callCCT :: (KT w m a -> ContT w m a) -> ContT w m a
-callCCT f = ContT $ \k -> unContT (f k) k
+callCCT :: (K r a -> Cont r a) -> Cont r a
+callCCT f = Cont $ \k -> unCont (f k) k
 
-throwT :: KT w m a -> a -> ContT w m b
-throwT k x = ContT $ \ _ -> k x
+throwT :: K r a -> a -> Cont r b
+throwT k x = Cont $ \ _ -> k x
 
 -- And here is the main difference from Cont: the higher-rank type of
 -- runCont, ensuring that the computation should really not have looked
 -- at the answer-type
-runContT :: Monad m => (forall w. ContT w m a) -> m a
-runContT m = unContT m return
+runContT :: Monad m => (forall w. Cont (m w) a) -> m a
+runContT m = unCont m return
 
-instance MonadTrans (ContT r) where
-    lift m = ContT (m >>=)
+runContT :: Monad m => (forall w. Cont (m w) a) -> m a
+runContT m = unCont m return
 
-instance (MonadIO m) => MonadIO (ContT r m) where
-    liftIO = lift . liftIO
+-- -- instance MonadTrans (ContT r) where
+-- --     lift m = ContT (m >>=)
+
+-- -- instance (MonadIO m) => MonadIO (ContT r m) where
+-- --     liftIO = lift . liftIO
 
 
--- type Cont w = ContT w Identity
+-- -- type Cont w = ContT w Identity
 
--- type K w a = a -> w
+-- -- type K w a = a -> w
 
--- callCC :: (K w a -> Cont w a) -> Cont w a
--- callCC f = Cont $ \k -> unCont (f k) k
+-- -- callCC :: (K w a -> Cont w a) -> Cont w a
+-- -- callCC f = Cont $ \k -> unCont (f k) k
 
--- throw :: K w a -> a -> Cont w b
--- throw k x = Cont $ \ _ -> k x
+-- -- throw :: K w a -> a -> Cont w b
+-- -- throw k x = Cont $ \ _ -> k x
+
+-- -- type ExtContT m a = forall w . ContT w m a
