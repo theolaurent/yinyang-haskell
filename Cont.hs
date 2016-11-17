e4c459db0e3fdcd9ef7838becafac9fb792c3a3d
Theo Laurent <theo.laurent@ens.fr>
2016-11-17 11:38:28 +0100

    More cleanup.

--- Cont.hs
+++ Cont.hs
@@ -4,13 +4,15 @@
 
 module Cont where
 
-import Control.Monad.IO.Class
-import Control.Monad.Trans.Class
 import Control.Monad (ap, liftM)
 
-import Control.Monad.Identity
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Trans.Class (MonadTrans, lift)
+import Control.Monad.Identity (Identity, runIdentity)
 
 newtype ContM w m a = ContM { unContM :: (a -> m w) -> m w }
+-- The higher-rank type ContM' ensures that the computation
+-- should not look at the answer-type
 type ContM' m a = forall w . ContM w m a
 
 instance Monad (ContM w m) where
@@ -25,9 +27,9 @@ instance Functor (ContM w m) where
   fmap = liftM
 
 
--- The type of the captured continuation
--- Once again, it is not a function in the object language;
--- it's result is NOT of the type Cont w x
+-- Type of captured continuations
+-- It is not a function in the object language;
+-- it's result is NOT of the type Cont w m x
 type K w m a = a -> m w
 
 callCC :: (K w m a -> ContM w m a) -> ContM w m a
@@ -36,20 +38,31 @@ callCC f = ContM $ \k -> unContM (f k) k
 throw :: K w m a -> a -> ContM w m b
 throw k x = ContM $ \ _ -> k x
 
--- And here is the main difference from Cont: the higher-rank type of
--- runCont, ensuring that the computation should really not have looked
--- at the answer-type
 runContM :: Monad m => ContM' m a -> m a
 runContM m = unContM m return
 
+-- Bringing IO to the Continuation Monad
 instance MonadTrans (ContM r) where
     lift m = ContM (m >>=)
 
 instance (MonadIO m) => MonadIO (ContM r m) where
     liftIO = lift . liftIO
 
+-- Simple Continuations
 type Cont w a = ContM w Identity a
 type Cont' a = forall w . Cont w a
 
 runCont :: Cont' a -> a
 runCont x = runIdentity $ runContM x
+
+-- excludedMiddle :: Cont' (Either (a -> Void) a)
+-- excludedMiddle = do
+--   let it :: Cont w (Either (a -> Cont w b) a)
+--       it = callCC (\ k -> throw k (Left (\ x -> throw k (Right x))))
+--   res <- it
+--   case res of
+--     Right a -> return $ Right a
+--     Left  f -> undefined
+-- To write this I would need the Falsum version and "dynamic check"
+-- that is continuation that are not type-guaranteed not to escape their scope
+-- (an alternative would be continuations that you can't run)
--- yinyang.hs
+++ yinyang.hs
@@ -10,41 +10,24 @@
 import Cont
 import Control.Monad.IO.Class (liftIO)
 
--- import Data.Void (Void)
-
 newtype RecK w m = RecK { unRecK :: K w m (RecK w m) }
 
-
--- excludedMiddle :: Cont' (Either (a -> Void) a)
--- excludedMiddle = do
---   let it :: Cont w (Either (a -> Cont w b) a)
---       it = callCC (\ k -> throw k (Left (\ x -> throw k (Right x))))
---   res <- it
---   case res of
---     Right a -> return $ Right a
---     Left  f -> undefined -- TODO: to write this I would need the Falsum version and "dynamic check"
---                          -- that is continuation that are not type-guaranteed not to escape their scope
---                          -- an alternative would be continuations that you can't run
-
-
 yinyang :: ContM' IO ()
-yinyang = do
+yinyang =
   let yin :: ContM w IO (RecK w IO)
       yin = do
         k <- callCC (\ k -> return $ RecK k)
-        liftIO $ print "@"
+        liftIO $ putChar '@'
         return k
-  -- ((lambda (cc) (display #\@) cc) (call-with-current-continuation (lambda (c) c)
-  -- (\ cc -> print "@" ; cc) (callCC id)
-  -- a -> a                  K (K (K ...))
-  let yang :: ContM w IO (RecK w IO)
+      yang :: ContM w IO (RecK w IO)
       yang = do
         k <- callCC (\ k -> return $ RecK k)
-        liftIO $ print "*"
+        liftIO $ putChar '*'
         return k
-  k1 <- yin
-  k2 <- yang
-  throw (unRecK k1) k2
+  in do
+    k1 <- yin
+    k2 <- yang
+    throw (unRecK k1) k2
 
 
 main :: IO ()
